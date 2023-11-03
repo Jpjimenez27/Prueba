165ff6e738a4bf53c6a2b9a936146d9366c7998f
Jpjimenez27 <124945451+Jpjimenez27@users.noreply.github.com>
2023-11-03 10:19:30 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDJ8moKvs_c27l3Iry5bOaOLFFLXWAWHHs",
+    authDomain: "gymm-9114b.firebaseapp.com",
+    projectId: "gymm-9114b",
+    storageBucket: "gymm-9114b.appspot.com",
+    messagingSenderId: "125962830426",
+    appId: "1:125962830426:web:8f5d7f2cb5146d86f30e63"
+  };
